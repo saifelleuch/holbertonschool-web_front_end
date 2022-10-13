7cec5bae79e2b338a7e7d7559f29ac3bff2514db
saifelleuch <sa2014if@gmail.com>
2022-10-13 20:44:05 +0100

    another one

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    var primes = 0;
+    var is_prime = true;
+    for (var counter = 2; counter <= 100; counter = counter + 1) {
+      is_prime = true;
+      var limit = Math.round(Math.sqrt(counter));
+      for (var mod = 2; mod <= limit; mod++) {
+        if (counter % mod == 0) {
+          is_prime = false;
+          break;
+        }
+      }
+      if (is_prime) {
+        primes = primes + 1;
+      }
+    }
+    return primes;
+  }
+  var inn = performance.now();
+  countPrimeNumbers();
+  var out = performance.now();
+  console.log(
+    'Execution time of printing countPrimeNumbers was ' +
+      (out - inn) +
+      ' milliseconds.'
+  );
