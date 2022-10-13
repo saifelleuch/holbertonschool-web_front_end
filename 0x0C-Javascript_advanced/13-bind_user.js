eac460185aaaae975f55038f7880f200f7c0528d
saifelleuch <sa2014if@gmail.com>
2022-10-13 23:18:17 +0100

    another one

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,21 @@
+var user = {
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Buillaume',
+    lastName: 'Ialva',
+    location: 'Telaviv',
+    occupation: 'Engineer',
+  };
+  function logWelcomeUser(welcomeString) {
+    console.log(
+      welcomeString +
+        ', ' +
+        this.firstName +
+        '.' +
+        ' Your occupation is: ' +
+        this.occupation
+    );
+  }
+  var bindLogWelcomeUser = logWelcomeUser.bind(user);
+  bindLogWelcomeUser('Welcome');
